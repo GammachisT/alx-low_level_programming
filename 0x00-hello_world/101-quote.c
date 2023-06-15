1deac364c85d3e1ffc1b66a6143d43c7b2f957f1
GammachisT <gammachistesfaye@gmail.com>
2023-06-15 06:14:51 -0700

    Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,12 @@
 
 /**
  * main - piece of art is useful
- * Return: 0 (success)
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
