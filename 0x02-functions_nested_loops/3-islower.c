24c76ab77cbb2ce2585bdca32dff792db89027f0
GammachisT <gammachistesfaye@gmail.com>
2023-06-20 06:48:43 -0700

    Function that checks for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - tests whether a charater is lower or not
+ * @c: character to test. 
+ *Return: 1 if c is lowercase, 0 if not.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
