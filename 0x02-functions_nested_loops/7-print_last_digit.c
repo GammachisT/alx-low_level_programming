06c1dc19135e174399dac4c55b2cea95850c3be2
GammachisT <gammachistesfaye@gmail.com>
2023-06-20 23:15:30 -0700

    computes the absolute value of an integer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,16 +7,11 @@
  */
 int print_last_digit(int n)
 {
-	int a;
-
+	n %= 10;
 	if (n < 0)
-	n = -n;
-	
-	a = n % 10;
-
-	if (a < 0)
-		a = -a;
-
-	_putchar(a + '0');
-	return (a);
+	{
+		n *= -1;
+	}
+	_putchar(n + '0');
+	return (n);
 }
