6097fa55405d28b877509e6fb300cadb16084144
GammachisT <gammachistesfaye@gmail.com>
2023-06-21 08:25:35 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 14)
 	{
 		for (i = 0; i <= n; i++)
@@ -20,7 +21,7 @@ void print_times_table(int n)
 					_putchar(32);
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');	
+					_putchar((k % 10) + '0');
 				}
 				else if (k > 9)
 				{
