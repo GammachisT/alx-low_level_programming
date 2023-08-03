433fd99e160c9687ee0caf0f37c1889376fec762
GammachisT <gammachistesfaye@gmail.com>
2023-08-03 07:51:57 -0700

    main

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _putchar(char c);
+int _putchar();
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
