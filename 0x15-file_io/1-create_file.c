616bb0fc85dbae53012fcc2348b21e74b376a32a
GammachiT <gammachistesfaye@gmail.com>
2023-09-05 11:28:08 -0700

    c file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * create_file - function that creates a file.
  * @filename: variable pointer
  * @text_content: content file
- * 
+ *
  * Return: 1 on success, -1 on failure
  */
 
@@ -20,8 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-
-	while (text_content[i] != '\0')
+	while (text_content[n] != '\0')
 	{
 		n++;
 	}
