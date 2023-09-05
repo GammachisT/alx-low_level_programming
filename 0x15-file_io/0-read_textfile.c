d5e97950cbd995e4b1bcf051dd1c42b0d46b89c1
GammachiT <gammachistesfaye@gmail.com>
2023-09-05 10:55:30 -0700

    c file i/o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - function that reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename: variable pointer
+ * @letters: size letters
+ * 
+ * Return: the actual number of letters it could read and print, 0 otherwise
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file, let, y;
+	char *text;
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+
+	let = read(file, text, letters);
+
+	y = write(STDOUT_FILENO, text, let);
+
+	close(file);
+
+	return (y);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_file - function that creates a file.
+ * @filename: variable pointer
+ * @text_content: content file
+ * 
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int n = 0, file;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
+	{
+		n++;
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, n);
+
+	return (1);
+}
